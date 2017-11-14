4ea7932c00ebad406dd957cb62542dd44b847cd7
unknown <firemed@hotmail.fr>
2017-11-14 18:14:11 +0100

    couleur typo Mehdi Aknouch + remake 'timeline-divider' + couleur items Expertise + fix surplus espaces 'Travaillons ensemble' + fix item menu actif

--- wp-content/themes/mehdipro/MAP/js/map.js
+++ wp-content/themes/mehdipro/MAP/js/map.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
     $('.navbar .nav li a').on('click', function() { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
+
+        if( page.length  == 0){
+            $('html, body').animate( { scrollTop: $('body').offset().top }, speed ); // Go
+            return false;
+        }
         console.log('smooth clicking to : '+page);
         var speed = 750; // Durée de l'animation (en ms)
         $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        $('.navbar .nav li').removeClass('active');
+        console.log('active removed');
+        $(this).parent().addClass('active');
+
         return false;
     });
 });
